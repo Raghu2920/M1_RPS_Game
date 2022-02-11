96b346ad8b2a7d3e88e26363dc12da3ab1d5f3be
Raghuram R <60956504+Raghu2920@users.noreply.github.com>
2022-02-12 01:54:11 +0530

    Create Greater.c

---
+++ 3_Implementation/source/Greater.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int greater(char c1, char c2)
+{
+    if (c1 == c2)
+    {
+        return -1;
+    }
+    else if (c1 == 'r' && c2 == 's')
+    {
+        return 1;
+    }
+    else if (c2 == 'r' && c1 == 's')
+    {
+        return 0;
+    }
+        else if (c1 == 'r' && c2 == 'p')
+    {
+        return 1;
+    }
+    else if (c2 == 'r' && c1 == 'p')
+    {
+        return 0;
+    }
+    else if (c1 == 's' && c2 == 'p')
+    {
+        return 1;
+    }
+    else if (c2 == 's' && c1 == 'p')
+    {
+        return 0;
+    }
+}
